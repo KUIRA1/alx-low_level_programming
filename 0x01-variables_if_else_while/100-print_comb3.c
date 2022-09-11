fef2ea47785bc11f5d7672a70c94d85316c72382
John kuira <johnnjengakuira@gmail.com>
2022-09-11 12:02:51 +0300

    ghhd

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,10 +16,8 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-
 			if (digit1 == 9 && digit2 == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
@@ -29,3 +27,5 @@ int main(void)
 
 	return (0);
 }
+Footer
+
